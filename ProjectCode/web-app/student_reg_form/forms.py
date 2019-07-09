bfc80c44a8f04fa5d59fa9fe6ad7a208942b68dc
Mekayel <mekayel.hosen@northsouth.edu>
2019-07-09 12:08:54 +0600

    Update forms.py
    solving #11 : create student registration form

--- ProjectCode/web-app/student_reg_form/forms.py
+++ ProjectCode/web-app/student_reg_form/forms.py
@@ -15,4 +15,13 @@ class StudentRegistrationForm(UserCreationForm):
             'password1',
             'password2'
         }
-    
+     def save(self, commit = True):
+        user = super(StudentRegistrationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
