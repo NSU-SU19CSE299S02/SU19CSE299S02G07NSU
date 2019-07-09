826eef6c639b945b928b8aa3047a94e9baf4ba28
Mekayel <mekayel.hosen@northsouth.edu>
2019-07-09 20:03:23 +0600

    edit urls
    correct the path

--- ProjectCode/web-app/readingroom/urls.py
+++ ProjectCode/web-app/readingroom/urls.py
@@ -18,5 +18,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('' , include('studen_reg_form.urls')),
+	path('' , include('student_reg_form.urls')),
 ]
