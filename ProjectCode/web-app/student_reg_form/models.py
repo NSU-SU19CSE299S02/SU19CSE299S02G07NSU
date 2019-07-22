7a618642bfe2f763f15be119a4cfe7433bda6ebb
Mekayel <mekayel.hosen@northsouth.edu>
2019-07-22 23:42:29 +0600

    Database #29 : pdf file upload
    1. database created for pdf file upload

--- ProjectCode/web-app/student_reg_form/models.py
+++ ProjectCode/web-app/student_reg_form/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# Database created for PDF file
+class pdf_file(models.Model):
+    name = models.CharField(max_length=100)
+    pdffile = models.FileField()
+
+    def __str__(self):
+        return self.name
