7db0cd7ca8911f8b38fe84b16defb3215022f8f3
Mekayel <mekayel.hosen@northsouth.edu>
2019-08-05 22:06:38 +0600

    Feature #35 : Update views.py
    1. Import templates view
    2. import httpResponse
    3. importe revrse and redirect

--- ProjectCode/web-app/student_reg_form/views.py
+++ ProjectCode/web-app/student_reg_form/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+
+
 
 # Create your views here.
