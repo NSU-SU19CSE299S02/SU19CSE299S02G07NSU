a974fbc6302b72347ed06265cd75d9a9053be90d
Mekayel <mekayel.hosen@northsouth.edu>
2019-08-05 22:24:32 +0600

    Feature #35 : update views.py file
    1. import authenticate login
    2. import student& teacher reg form
    3. import pdf file and upload function

--- ProjectCode/web-app/student_reg_form/views.py
+++ ProjectCode/web-app/student_reg_form/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import StudentRegistrationForm,Teachers_Signing_Form
+from django.core.files.storage import FileSystemStorage
 
+from .models import pdf_file
+from .forms import uploadpdf 
 
 
 # Create your views here.
