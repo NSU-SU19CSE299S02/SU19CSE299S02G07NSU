4fff5f17b63e8c8e990187d2088be9065b3e4ca5
Mekayel <mekayel.hosen@northsouth.edu>
2019-08-06 08:47:10 +0600

    Feature #35 : update student login function
    1. set return to homepage
    2. set return to mainpage

--- ProjectCode/web-app/student_reg_form/views.py
+++ ProjectCode/web-app/student_reg_form/views.py
@@ -38,3 +38,9 @@ def student_login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
+		if user:
+             login(request, user)
+             return redirect("mainpage")
+          
+        else:
+             return render(request, "home.html")
