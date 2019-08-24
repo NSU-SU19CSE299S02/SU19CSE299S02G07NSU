baf438cb33177dbe5e3711b7f8962a59ce5d754b
Mekayel <mekayel.hosen@northsouth.edu>
2019-08-25 00:14:52 +0600

    Feature #38 : create teacher sign in form
    1. create class for teacher sign in form
    2. set the field name of form

--- ProjectCode/web-app/student_reg_form/forms.py
+++ ProjectCode/web-app/student_reg_form/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import pdf_file,TeachersInfo
 
 # This class for student registration form which data saved into database
 class StudentRegistrationForm(UserCreationForm):
@@ -15,6 +16,7 @@ class StudentRegistrationForm(UserCreationForm):
             'password1',
             'password2'
         }
+		
      def save(self, commit = True):
         user = super(StudentRegistrationForm, self).save(commit=False)
         user.first_name = self.cleaned_data['first_name']
@@ -25,3 +27,11 @@ class StudentRegistrationForm(UserCreationForm):
             user.save()
 
         return user
+		
+# This class for Teacher registration form which data saved into database
+class Teachers_Signing_Form(forms.ModelForm):
+
+
+    class Meta:
+        model = TeachersInfo
+        fields = ('Username','First_Name','Last_Name','Email','Password','Confirm_Password',) 
