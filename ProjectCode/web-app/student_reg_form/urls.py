f9555e807ddefd7300773411da80575238f0dce6
Mekayel <mekayel.hosen@northsouth.edu>
2019-08-27 02:57:50 +0600

    Feature #39 : update urls.py
    1. update urls
    2. fixup view file
    3. fixup htnl file

--- ProjectCode/web-app/student_reg_form/urls.py
+++ ProjectCode/web-app/student_reg_form/urls.py
@@ -4,14 +4,13 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-	path('', views.success, name = "homepage"),
+    path('', views.success, name = "homepage"),
     path('studentform/', views.studentform, name='student_form'),
     path('teacherform/', views.teacher_form, name='TeacherForm'),
     path('login/', views.student_login, name ="login"),
     path('accounts/profile/', views.pdf_view, name = "mainpage"),
     path('aaa/', views.pdf_up, name = "pdf_upload_page"),
     path('teacher_login/', views.teacher_login_form, name ="teacher_login"),
-    path('abc/', views.image_capture, name="image"),
 ]
 
 if settings.DEBUG:
