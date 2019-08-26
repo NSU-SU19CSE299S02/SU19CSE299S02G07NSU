87a2fd9484b82673c2f4c8e7d1e4c5b7c9964cea
Mekayel <mekayel.hosen@northsouth.edu>
2019-08-27 00:58:44 +0600

    Feature #39 : update urls.py
    1. import different function
    2. connect html page with urls

--- ProjectCode/web-app/student_reg_form/urls.py
+++ ProjectCode/web-app/student_reg_form/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    #path('',),
+	path('', views.success, name = "homepage"),
+    path('studentform/', views.studentform, name='student_form'),
+    path('teacherform/', views.teacher_form, name='TeacherForm'),
+    path('login/', views.student_login, name ="login"),
+   
 
 ]
+
